b3dae96ad0a090bd7ccd1b4a97e5750c712944f2
Toshakins <toshakins@gmail.com>
2013-10-22 01:25:44 +0300

    Initial Commit

---
+++ coconaut/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  coconautAppController.h
+//  coconaut
+//
+//  Created by Watta Puck on 10/22/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ coconaut/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  coconautAppController.h
+//  coconaut
+//
+//  Created by Watta Puck on 10/22/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
